9950625069a4fc3243eaf63fecff65d2c1809c80
kaluu89 <lukakulic989@gmail.com>
2024-02-19 12:09:31 +0100

    ispravljen bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,6 @@
 <h3>Kviz</h3>
 <Question
     question="Who is the main character with yellow hair in the anime Naruto?"
-    correctAnswer="Naruto";
-    incorrectAnswers={"Ten Ten","Sasuke","Kakashi"};
+    correctAnswer="Naruto"
+    incorrectAnswers={["Ten Ten","Sasuke","Kakashi"]}
 />
--- src/routes/question.svelte
+++ src/routes/question.svelte
@@ -2,9 +2,9 @@
     export let question;
     export let correctAnswer;
     export let incorrectAnswers;
-    const shuffle=(array)=>array.sort(()=>Math.random-0.5);
+    const shuffle=(array)=>{return array.sort(()=>Math.random()-0.5)};
     let possibleAnswers=shuffle([...incorrectAnswers,correctAnswer]);
-    let correctIndex= possibleAnswers.indexOf(correctAnswer);
+    
     const handleCLick=(ans)=>{
         if(ans==correctAnswer){
             alert("Tacno!");
@@ -19,7 +19,7 @@
     <h3>{@html question}</h3>
     <div id="answers">
         {#each possibleAnswers as pa,i}
-        <button on:click={handleCLick(pa)}>pa</button>
+        <button on:click={()=>handleCLick(pa)}>{pa}</button>
         {/each}
     </div>
 </div>
