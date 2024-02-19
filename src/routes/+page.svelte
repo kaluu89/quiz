275878a39f18845c57e3c8572b1bda4ba04a6c9a
kaluu89 <lukakulic989@gmail.com>
2024-02-19 12:02:39 +0100

    prezentaciona logika

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Question from "./question.svelte";
+</script>
+<h3>Kviz</h3>
+<Question
+    question="Who is the main character with yellow hair in the anime Naruto?"
+    correctAnswer="Naruto";
+    incorrectAnswers={"Ten Ten","Sasuke","Kakashi"};
+/>
---
+++ src/routes/question.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let question;
+    export let correctAnswer;
+    export let incorrectAnswers;
+    const shuffle=(array)=>array.sort(()=>Math.random-0.5);
+    let possibleAnswers=shuffle([...incorrectAnswers,correctAnswer]);
+    let correctIndex= possibleAnswers.indexOf(correctAnswer);
+    const handleCLick=(ans)=>{
+        if(ans==correctAnswer){
+            alert("Tacno!");
+        }
+        else{
+            alert(`Netacan odgovor! Tacan je ${correctAnswer}`); 
+        }
+    }
+</script>
+
+<div>
+    <h3>{@html question}</h3>
+    <div id="answers">
+        {#each possibleAnswers as pa,i}
+        <button on:click={handleCLick(pa)}>pa</button>
+        {/each}
+    </div>
+</div>
